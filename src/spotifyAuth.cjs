fc66e8b7b01230442b67e228a09fbf6dd6183f7e
Priya <ganapathipriyadharsani@gmail.com>
2025-07-06 17:41:51 +0530

    add auth

---
+++ src/spotifyAuth.cjs
@@ -0,0 +1,20 @@
+
+function generateCodeVerifier(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+  
+  async function generateCodeChallenge(codeVerifier) {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=+$/, '');
+  }
+  
+  export { generateCodeVerifier, generateCodeChallenge };
